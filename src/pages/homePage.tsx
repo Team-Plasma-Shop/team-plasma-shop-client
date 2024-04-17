7707faa99a5f094d507a6d23e8d1a74c70d431d2
Val <pro.mvaldo@gmail.com>
2024-04-17 15:55:52 +0200

    :sparkles: feat : homepage intro text

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,8 +1,11 @@
 function HomePage() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    
+      <section className="mt-36">
+            <h1 className="text-6xl font-semibold">En stock</h1>
+            <p className="text-xl w-2/4 mt-5 opacity-60">Volés avec respect, vendus pour l'argent : découvrez et adoptez votre Pokémon idéal avec Team Plasma !</p>
+        </section>
+    
   );
 }
 
