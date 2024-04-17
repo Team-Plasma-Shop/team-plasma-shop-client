44f9cdd140700e10f6cef3d1736958ec0bbfe1f6
Mathis Zerari <mathiszerari@icloud.com>
2024-04-17 11:38:43 +0200

    :art: pokemon model fix

--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -1,30 +1,12 @@
 export
 interface Pokemon {
   uuid: string;
-
-
-
-
-
-
-
   name: string;
-  imageLink       : string;
+  imageLink: string;
   price: number;
-
-
-
-
-  
-  type   :     string;
-  owner: string     ;
-  isSold    : boolean
+  type: string;
+  owner: string;
+  isSold: boolean
   createdAt: Date;
-
-
-
-
-
-
-  modifiedAt: Date
+  modifiedAt: Date;
 }
