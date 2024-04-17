26b17cfc0892d89102f5f992ef1ae69e6741247d
Mathis Zerari <mathiszerari@icloud.com>
2024-04-17 20:17:53 +0200

    :construction: totp page in dev

--- src/pages/signupPage.tsx
+++ src/pages/signupPage.tsx
@@ -6,7 +6,7 @@ function SignupPage() {
   return (
     <div className="flex flex-row justify-between">
       <div className="mt-28 mx-auto">
-        <h1 className="text-4xl px-4 mb-10">Faisons affaire !</h1>
+        <h1 className="text-4xl pr-8 mb-10">Faisons affaire !</h1>
         <SignupForm />
       </div>
 
