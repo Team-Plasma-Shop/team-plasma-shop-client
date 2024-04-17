1a28e3c927762180b54b21bc87e340224dad21fc
Val <pro.mvaldo@gmail.com>
2024-04-17 14:56:26 +0200

    :sparkles: feat : pages header

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,20 @@ import HomePage from './pages/homePage';
 import LoginPage from './pages/loginPage';
 import AboutPage from './pages/aboutPage';
 import SignupPage from './pages/signupPage';
+import Header from './components/header';
+import AccountPage from './pages/accountPage';
 
 function App() {
   return (
     <div>
       <Router>
+      <Header></Header>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/account" element={<AccountPage/>} />
         </Routes>
       </Router>
     </div>
