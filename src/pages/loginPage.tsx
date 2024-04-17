035e379ab12ee4e657fed92b96c2389d23157dad
Mathis Zerari <mathiszerari@icloud.com>
2024-04-17 15:05:24 +0200

    :art: fix initilization of components

--- src/pages/loginPage.tsx
+++ src/pages/loginPage.tsx
@@ -1,20 +1,18 @@
-import { Component } from "react";
-
 import { PlasmaLogo } from "../assets/PlasmaLogo";
 
 import LoginForm from "../components/loginForm";
 
-export default class LoginPage extends Component {
-  public render() {
-    return (
-      <div className="flex flex-row justify-between">
-        <div className="mt-28 mx-auto">
-          <h1 className="text-5xl mb-10">Connexion</h1>
-          <LoginForm />
-        </div>
-
-        <PlasmaLogo />
+function LoginPage() {
+  return (
+    <div className="flex flex-row justify-between">
+      <div className="mt-28 mx-auto">
+        <h1 className="text-5xl mb-10">Connexion</h1>
+        <LoginForm />
       </div>
-    );
-  }
+
+      <PlasmaLogo />
+    </div>
+  );
 }
+
+export default LoginPage
