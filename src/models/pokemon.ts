9096801062d49bf138683cd98b9c283b0d194917
Mathis Zerari <mathiszerari@icloud.com>
2024-04-17 11:32:39 +0200

    :bug: correct pokemon interface

---
+++ src/models/pokemon.ts
@@ -0,0 +1,11 @@
+export interface Pokemon {
+  uuid: string;
+  name: string;
+  imageLink: string;
+  price: number;
+  type: string;
+  owner: string;
+  isSold: boolean
+  createdAt: Date;
+  modifiedAt: Date
+}
