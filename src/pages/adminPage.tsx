55cfe3f1dc01174bda5d877638461802c1b6e880
Mathis Zerari <mathiszerari@icloud.com>
2024-04-18 21:25:47 +0200

    :construction: admin page created

---
+++ src/pages/adminPage.tsx
@@ -0,0 +1,12 @@
+
+function adminPage() {
+  return (
+    <div className="flex flex-row justify-between">
+      <div className="mt-28 mx-auto">
+        <h1 className="text-4xl pr-24 mb-10">Admin Dashboard</h1>
+      </div>
+    </div>
+  );
+}
+
+export default adminPage
