0ad9771a5a0f828954ba2807a43b871d73fe669a
Mathis Zerari <mathiszerari@icloud.com>
2024-04-18 22:13:09 +0200

    :construction: admin route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import AccountPage from './pages/accountPage';
 import TOTPPage from './pages/totpPage';
 import PrivateRoutes from './utils/PrivateRoute';
 import VerificationPage from "./pages/verificationPage";
+import AdminPage from './pages/adminPage';
 
 function App() {
   return (
@@ -19,12 +20,13 @@ function App() {
           <Route element={<PrivateRoutes/>}>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
-            <Route path="/account" element={<AccountPage/>} />
+            <Route path="/account" element={<AccountPage />} />
+            <Route path="/admin" element={<AdminPage />}></Route>
           </Route>
           
+          <Route path="/email-verification" element={<VerificationPage />}></Route>
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/email-verification" element={<VerificationPage />}></Route>
           <Route path="/totp" element={<TOTPPage />} />
         
         </Routes>
