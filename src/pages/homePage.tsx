185802e78e6b527f31a51b4908cbfbfaa97f93b0
Mathis Zerari <mathiszerari@icloud.com>
2024-04-18 21:30:42 +0200

    :bug: fix bug bc of models changes

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -9,36 +9,33 @@ function HomePage() {
 
   const pokemons: Pokemon[] = [
     {
-      uuid: '1',
+      id: '1',
       name: 'Bulbasaur',
       imageLink: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png',
       price: 100,
       type: 'Grass',
       owner: 'Ash Ketchum',
       isSold: false,
-      createdAt: new Date(),
       modifiedAt: new Date(),
     },
     {
-      uuid: '2',
+      id: '2',
       name: 'Charmander',
       imageLink: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png',
       price: 150,
       type: 'Fire',
       owner: 'Gary Oak',
       isSold: false,
-      createdAt: new Date(),
       modifiedAt: new Date(),
     },
     {
-      uuid: '3',
+      id: '3',
       name: 'Squirtle',
       imageLink: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png',
       price: 120,
       type: 'Water',
       owner: 'Misty',
       isSold: true,
-      createdAt: new Date(),
       modifiedAt: new Date(),
     },
   ];
@@ -60,7 +57,7 @@ function HomePage() {
             <div className="grid grid-cols-4 mt-8">
             {
               pokemons.map((pokemon) => {
-                return <PokemonCard key={pokemon.uuid} pokemon={pokemon}></PokemonCard>
+                return <PokemonCard key={pokemon.id} pokemon={pokemon}></PokemonCard>
               } )
             }
             </div>
