e30414f923ecbaf6a831700304061072f663cccd
Mathis Zerari <mathiszerari@icloud.com>
2024-04-18 23:57:49 +0200

    Revert ":construction: modal edit pokemon is building"
    This reverts commit 814e48ba8350fb351bd2ec915a0c49747ec73570.

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,12 @@ function App() {
       <Header></Header>
         <Routes>
           <Route element={<PrivateRoutes/>}>
+            <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/account" element={<AccountPage />} />
           </Route>
           
-          <Route path="/" element={<HomePage />} />
-          <Route path="/admin" element={<AdminPage />} />
-
-
+          <Route path="/admin" element={<AdminPage />}></Route>
           <Route path="/email-verification" element={<VerificationPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
